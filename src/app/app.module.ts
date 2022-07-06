4c2405ecb7193116ffd0aad0126b4166ade9013a
AngularDeveloperUI <rajesh.frontenddev@gmail.com>
2022-07-06 15:27:51 +0530

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialExampleModule} from '../material.module';
+import { PrimaryNavComponent } from './primary-nav/primary-nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrimaryNavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
