cea41d6af9461852adb7445bafbcf452ac26e890
Jagadeesh0608 <jagadeesh.jorepalli@gmail.com>
2022-07-08 20:32:36 +0530

    jagadeesh changes

--- src/app/billpayment/billpayment.component.ts
+++ src/app/billpayment/billpayment.component.ts
@@ -24,21 +24,47 @@ export class BillpaymentComponent {
     secondCtrl: ['', Validators.required],
   });
   thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required],
+    amount: ['', Validators.required],
   });
   fourFormGroup = this._formBuilder.group({
-    fourCtrl: ['', Validators.required],
-    fifthCtrl: ['', Validators.required],
+    cardNum: ['', Validators.required],
+    password: ['', Validators.required],
   });
   stepperOrientation: Observable<StepperOrientation>;
-
-
-  ngOnInit(): void {
-  }
-
   constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver,) {
     this.stepperOrientation = breakpointObserver
       .observe('(min-width: 800px)')
       .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
   }
+  userData:any;
+  parsedData:any;
+  cardsData:any;
+  rechargeStatus:boolean;
+  rechargedAmount:number;
+  ngOnInit(): void {
+    this.userData = sessionStorage.getItem('currentData')
+    this.parsedData = JSON.parse(this.userData);
+    this.cardsData = this.parsedData.cards
+  }
+  onPayment(form:any){
+    this.rechargedAmount = form.value.amount
+  }
+  onRecharge(form:any) {
+    console.log(form.value);
+    this.cardsData.forEach((element: any) => {
+      if(form.value.cardNum === element.cardNum) {
+        if(form.value.password === element.cardPin){
+           this.rechargeStatus = true
+           this.parsedData.accounts[0].accountBalance = this.parsedData.accounts[0].accountBalance-this.rechargedAmount
+           this.parsedData.accounts[1].accountBalance = this.parsedData.accounts[1].accountBalance-this.rechargedAmount
+           sessionStorage.setItem("currentData", JSON.stringify(this.parsedData))
+        } else{
+          alert('Please enter Valid Pin....');
+          this.rechargeStatus = false
+        }
+      }
+    });
+  }
+
+  
 }
